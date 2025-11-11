332f8a51c45e9eb40846252e592328584231a388
KilianSchmitt <116954063+KilianSchmitt@users.noreply.github.com>
2025-11-11 10:37:22 +0100

    M6: Added helm, terraform etc in extras and changed paths in deployment.yml etc.

--- src/main/java/com/acme/kms/repository/MockDB.java
+++ src/main/java/com/acme/kms/repository/MockDB.java
@@ -18,10 +18,13 @@ public class MockDB {
     static {
         final Kassierer kassierer1 = new Kassierer(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"), "Kilian Schmitt");
         final Kassierer kassierer2 = new Kassierer(UUID.fromString("123e4567-e89b-12d3-a456-426614174001"), "Herr Zimmermann");
+        final Kassierer kassierer3 = new Kassierer(UUID.fromString("123e4567-e89b-12d3-a456-426614174002"), "Frau Müller");
 
         final KassenBon kassenBon1 = new KassenBon(UUID.fromString("123e4567-e89b-12d3-a456-426614174321"), LocalDate.of(2025, 1, 29), new BigDecimal("50.80"));
         final KassenBon kassenBon2 = new KassenBon(UUID.fromString("123e4567-e89b-12d3-a456-425814174001"), LocalDate.of(2025, 1, 29), new BigDecimal("300"));
         final KassenBon kassenBon3 = new KassenBon(UUID.fromString("123e4567-e89b-12d3-a456-444614174001"), LocalDate.of(2025, 1, 29), new BigDecimal("0.98"));
+        final KassenBon kassenBon4 = new KassenBon(UUID.fromString("123e4567-e89b-12d3-a456-426614174322"), LocalDate.of(2025, 2, 1), new BigDecimal("120.50"));
+        final KassenBon kassenBon5 = new KassenBon(UUID.fromString("123e4567-e89b-12d3-a456-426614174323"), LocalDate.of(2025, 2, 2), new BigDecimal("15.75"));
 
         final Kasse kasse1 = new KasseBuilder()
             .withId(UUID.fromString("00000000-0000-0000-0000-000000000001"))
@@ -36,6 +39,14 @@ public class MockDB {
             .addBon(kassenBon3)
             .build();
 
+        final Kasse kasse3 = new KasseBuilder()
+                .withId(UUID.fromString("00000000-0000-0000-0000-000000000003"))
+                .withKassierer(kassierer3)
+                .addBon(kassenBon4)
+                .addBon(kassenBon5)
+                .build();
+
+        KASSEN.add(kasse3);
         KASSEN.add(kasse1);
         KASSEN.add(kasse2);
     }
