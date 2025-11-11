2a4c996cbf55f3b0388c87dd11d9c8a1c34e9721
KilianSchmitt <116954063+KilianSchmitt@users.noreply.github.com>
2025-11-11 11:28:10 +0100

    Add error handling for "Kasse not found" (404) on path parameter

--- src/main/java/com/acme/kms/service/NotFoundException.java
+++ src/main/java/com/acme/kms/service/NotFoundException.java
@@ -1,12 +1,7 @@
 package com.acme.kms.service;
 
-import java.io.Serial;
-
 public final class NotFoundException extends RuntimeException {
-    @Serial
-    private static final long serialVersionUID = 1101909572340666200L;
-
     public NotFoundException() {
-        super("Es wurde keine Kasse gefunden.");
+        super("Es wurde keine Kasse gefunden. Fehlercode: 404");
     }
 }
